bef1b7a8e45810c82b6573af031948844acd5bf5
Murat Karasah <mkarasah@gmail.com>
2021-12-03 09:07:57 +0300

    Add project files.

---
+++ multiArrayConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace multiArrayConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] bolgeler = new string[7, 3]
+            {
+                { "İstanbul","çanakkale","bursa" },
+                { "izmir","manisa","muğla" },
+                { "antalya","ısparta","mersin" },
+                { "konya","ankara","eskişehir" },
+                { "zonguldak","trabzon","rize" },
+                { "gaziantep","mardin","şanlıurfa" },
+                { "ağrı","hakkari","van" }
+            };
+
+            for (int i = 0; i <= bolgeler.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= bolgeler.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(bolgeler[i,j]);
+                }
+                Console.WriteLine("***");
+
+            }
+
+        }
+    }
+}
